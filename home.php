90db67bd08401d45c418bca5d94292f99840ee35
JuanGz330 <juangz3300@gmail.com>
2025-09-24 13:26:32 -0500

    home y index

--- home.php
+++ home.php
@@ -11,7 +11,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 	<!-- FontAwesome -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
 
 	<!-- jQuery -->
 	<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
