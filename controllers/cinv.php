6fc2aed9c67f7d679e35d97c20aedee4193bca84
GridTech95 <gridtech95@gmail.com>
2025-10-29 13:57:22 -0500

    Update cinv.php

--- controllers/cinv.php
+++ controllers/cinv.php
@@ -2,6 +2,7 @@
 require_once('models/minv.php');
 
 $minv = new MInv();
+
 $idinv = isset($_REQUEST['idinv']) ? $_REQUEST['idinv']:NULL;
 $idprod = isset($_POST['idprod']) ? $_POST['idprod']:NULL;
 $idubi = isset($_POST['idubi']) ? $_POST['idubi']:NULL;
@@ -11,6 +12,7 @@ $fec_actu = isset($_POST['fec_actu']) ? $_POST['fec_actu']:NULL;
 
 $ope = isset($_REQUEST['ope']) ? $_REQUEST['ope']:NULL;
 $datOne = NULL;
+
 $minv->setIdinv($idinv);
 if($ope == "save"){
     $minv->setIdprod($idprod);
@@ -18,10 +20,24 @@ if($ope == "save"){
     $minv->setCant($cant);
     $minv->setFec_crea($fec_crea);
     $minv->setFec_actu($fec_actu);
-    if(!$idinv) $minv->save(); else $minv->upd();
+    // Guardar o actualizar
+    if ($idinv) {
+        $minv->upd();
+        echo "<script>window.location.href = 'home.php?pg=$pg&msg=updated';</script>";
+        exit;
+    } else {
+        $minv->save();
+        echo "<script>window.location.href = 'home.php?pg=$pg&msg=saved';</script>";
+        exit;
+    }
 }
 
-if($ope =="eli" && $idinv) $minv->del();
+if ($ope == "eli" && $idinv) {
+    $minv->setIdinv($idinv);
+    $minv->del();
+    echo "<script>window.location.href = 'home.php?pg=$pg&msg=deleted';</script>";
+    exit;
+}
 if($ope =="edi" && $idinv) $datOne = $minv->getOne();
 
 $datAll = $minv->getAll();
